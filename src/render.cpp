dfacb933c4b6b49aafb575c3ff4502f1085604e4
SylvKT <contact@sylv.gay>
2024-02-06 19:18:56 -0500

    add clarifying comments

--- src/render.cpp
+++ src/render.cpp
@@ -10,9 +10,10 @@
 #include "shader.hpp"
 
 const float vertices[] = {
-     0.5f, -0.5f, 0.0f, 1.0f,  0.0f,  0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, 0.0f,  1.0f,  0.0f, // bottom left
-     0.0f,  0.5f, 0.0f, 0.0f,  0.0f,  1.0f, // top right
+    // vertices         // color
+     0.5f, -0.5f, 0.0f, 1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f,  1.0f,  0.0f,
+     0.0f,  0.5f, 0.0f, 0.0f,  0.0f,  1.0f,
 };
 
 bool render_init(RenderVars *render_vars) {
